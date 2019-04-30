b6e3bfe66ff7176c3d553b30d691a5fb206f1832
oscar <oscarros@kth.se>
2019-04-30 11:10:36 +0200

    add requires in install

--- build_wrapper/setup.py
+++ build_wrapper/setup.py
@@ -2,6 +2,9 @@ from setuptools import setup
 setup(
     name='bscan',
     version='0.0.1',
+    install_requires=[
+        'requests',
+    ],
     entry_points={
         'console_scripts': [
             'bscan=bscan:run'
